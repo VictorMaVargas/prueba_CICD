c3cec72aafde62a05cb657412fa1276bc6257389
VictorMaVargas <victormavargas@gmail.com>
2021-07-16 12:02:12 +0200

    ok test

--- math.test.js
+++ math.test.js
@@ -1,7 +1,7 @@
 const { sum, mul, sub, div } = require('./math');
 
 test('Adding 1 + 1 equals 2', () => {
-  expect(sum(1, 1)).toBe(2000);
+  expect(sum(1, 1)).toBe(2);
 });
 test('Multiplying 1 * 1 equals 1', () => {
   expect(mul(1, 1)).toBe(1);
